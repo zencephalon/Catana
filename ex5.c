7743938491db4bfc7713bf389a5258b213ba12b9
Matthew Bunday <zenceph@gmail.com>
2014-11-10 16:53:39 -0500

    Add exercise 5.

---
+++ ex5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* A comment here, my good man. */
+int main(int argc, char *argv[]) {
+  int distance = 1337;
+
+  // Another type of comment.
+  printf("I find U %d miles away.\n", distance);
+
+  return 0;
+}
